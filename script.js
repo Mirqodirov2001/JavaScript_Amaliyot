b184de63485f0b557bc0af2827fe98ae5cd01892
mirqodirov2001 <mirqodirov678@gmail.com>
2023-06-07 12:10:07 +0500

    Amaliyot

---
+++ script.js
@@ -0,0 +1,33 @@
+//arr masala
+
+// let ishchilar = [
+//     { name: `Elnazar`, age: 19, email: `eln@gmail.com` },
+//     { name: `Jonnazar`, age: 25, email: `jon@gmail.com` },
+//     { name: `Murodnazar`, age: 27, email: `mur@gmail.com` },
+//     { name: `Nazar`, age: 29, email: `naz@gmail.com` },
+// ];
+
+// for (let i = 0; i < ishchilar.length; i++)
+// {
+//     if (ishchilar[i].age <= 20)
+//     {
+//         console.log(ishchilar[i].name)
+//     }
+// }
+
+//Ishchilar orasidan eng yoshini chiqarish
+
+// let ishchilar = [
+//     { name: `Elnazar`, age: 19, email: `eln@gmail.com` },
+//     { name: `Jonnazar`, age: 25, email: `jon@gmail.com` },
+//     { name: `Murodnazar`, age: 27, email: `mur@gmail.com` },
+//     { name: `Nazar`, age: 29, email: `naz@gmail.com` },
+// ];
+
+// for (const value of ishchilar)
+// {
+//     if (value.age <= 19)
+//     {
+//         console.log(value)
+//     }
+// }
